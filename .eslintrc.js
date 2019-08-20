4cd3c69c2232f63fd69cb266a66567ec1f7926f3
halafi <filiphalas74@gmail.com>
2019-08-20 12:54:11 +0200

    fix(parser):

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,17 @@
 module.exports = {
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  plugins: ['react', 'jsx-a11y', 'import', 'prettier'],
   env: {
     browser: true,
     es6: true,
     node: true,
     jest: true,
   },
-  extends: ['airbnb', 'prettier', 'prettier/react'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -17,7 +19,6 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react', 'jsx-a11y', 'import', 'prettier'],
   rules: {
     'prettier/prettier': 'error',
     'import/no-extraneous-dependencies': [
